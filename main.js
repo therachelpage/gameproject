163a615ba05549e23476c4f652fbf3eac56f41d8
Rachel <punkrockevolution@gmail.com>
2016-12-01 11:46:23 -0800

    first draft of game

--- main.js
+++ main.js
@@ -2,3 +2,9 @@ var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
+
+if (cardTwo === cardFour) {
+	alert('You found a match!');	
+} else {
+  alert('Sorry, try again.');
+}
